f87b57c161bcab02bb468ad2ab5d6404ae414d7b
Dongfu Jiang <82598202+jdf-prog@users.noreply.github.com>
2023-09-19 11:15:40 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author="Your Name",
+    author_email="your@email.com",
+    description="A short description of your package",
+    # Add other metadata as needed
+)
